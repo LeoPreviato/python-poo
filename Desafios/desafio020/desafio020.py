adac8752095677c4223d5eb10aa710c99786568c
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-04-03 18:56:19 -0300

    refactor(desafio020): adiciona docstrings e refatora adicionar_jogo

--- Desafios/desafio020/desafio020.py
+++ Desafios/desafio020/desafio020.py
@@ -2,16 +2,41 @@ from rich import print
 from rich.panel import Panel
 
 class Gamer:
+    """
+    Classe que representa um jogador de vídeo games.
+
+    Atributos:
+        nome (str): Nome real do jogador.
+        nome_de_jogo (str): Nickname ou apelido do jogador.
+        jogos (list): Lista de jogos favoritos do jogador.
+    """
+
     def __init__(self, nome, nickname):
+        """
+        Inicializa um novo objeto Gamer.
+
+        Args:
+            nome (str): Nome real do jogador.
+            nickname (str): Nome de jogo (apelido) do jogador.
+        """
         self.nome = nome
         self.nome_de_jogo = nickname
-        self.jogos = []
+        self.jogos = list()
 
-    def adicionar_jogo(self, *jogo):
-        for jogos in jogo:
-            self.jogos.append(jogos)
+    def adicionar_jogo(self, games):
+        """
+        Adiciona um jogo à lista de favoritos do jogador e ordena alfabeticamente.
+
+        Args:
+            games (str): Nome do jogo a ser adicionado.
+        """
+        self.jogos.append(games)
+        self.jogos = sorted(self.jogos, key=str.lower)
 
     def ficha(self):
+        """
+        Exibe a ficha do jogador com seus dados e lista de jogos favoritos em um painel formatado.
+        """
         lista_jogos = "\n:video_game: ".join(self.jogos)
         print(Panel(
             f"Nome real: [black on blue]{self.nome}[/]\nJogos favoritos:\n:video_game: [blue]{lista_jogos}[/]",
